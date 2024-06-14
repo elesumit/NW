4d99c9246a1aa7a3515855ea54597919678451e6
Gupta, Sumit <GUPTS71@nationwide.com>
2024-06-13 23:48:01 -0400

    code 613

--- cypress/support/linkHelper.js
+++ cypress/support/linkHelper.js
@@ -1,11 +1,10 @@
 // cypress/support/linkHelper.js
-//const pageControllers = require('../../pages/pageControllers.js');
 
 const pageControllers = require('../../cypress/pages/pageControllers.js');
 
 export function handleLinkValidation(childRows, currentPageUrl) {
     childRows.forEach((childRow) => {
-        cy.log(childRow['expectedText'], childRow['condition']);
+       // cy.log(childRow['expectedURL'], childRow['condition']);
 
         // Get the XPath for the given condition and link
         const xpath = pageControllers.getXPath(childRow['condition'], childRow['linkText']);
@@ -14,7 +13,18 @@ export function handleLinkValidation(childRows, currentPageUrl) {
         if (xpath) {
             pageControllers.clickLink(xpath);
             cy.wait(2000);
-            cy.contains(childRow['expectedText']).should('be.visible');
+            //cy.contains(childRow['expectedText']).should('be.visible');
+
+            cy.url().then((url) => {
+                cy.title().then((title) => {
+                 if (url==childRow['expectedURL'])
+                  cy.title().should('eq', childRow['expectedTitle']);
+
+                
+                 
+                });
+              });
+
             cy.visit(currentPageUrl);
         } else {
             cy.log(`No matching XPath for condition: ${childRow['condition']}`);
